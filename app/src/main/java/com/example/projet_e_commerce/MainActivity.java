0afd4900b60b20c57394a767cee2112954c60ffb
jean-noel18 <jean-noel.nallamoutou@outlook.fr>
2021-11-29 15:57:49 +0100

    ajout acceuil

--- app/src/main/java/com/example/projet_e_commerce/MainActivity.java
+++ app/src/main/java/com/example/projet_e_commerce/MainActivity.java
@@ -9,35 +9,15 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button boutonGestionnaire;
-    Button boutonCommercant;
-    Button boutonClient;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        boutonGestionnaire = findViewById(R.id.buttonGestionnaire);
-        boutonCommercant = findViewById(R.id.buttonCommercant);
-        boutonClient = findViewById(R.id.buttonClient);
 
-        boutonGestionnaire.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                pageConnexion();
-            }
-        });
     }
 
-    public void pageConnexion(){
 
-        Intent intent = new Intent(this, ConnexionClientActivity.class);
-
-        /*EditText champDomaine = (EditText) findViewById(R.id.editTextDomaineCompetence);
-        String valeurDomaine = champDomaine.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE_DOMAINE_COMPETENCE,valeurDomaine);
-*/
-        startActivity(intent);
-    }
 }
