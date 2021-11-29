1345781a5caf67a4c06963e5b3f7cad91006fe88
ewfaye <eliewafaye@gmail.com>
2021-11-29 15:48:38 +0100

    Intent pour l'activité ConnexionCommercantActivity

--- app/src/main/java/com/example/projet_e_commerce/ConnexionCommercantActivity.java
+++ app/src/main/java/com/example/projet_e_commerce/ConnexionCommercantActivity.java
@@ -2,13 +2,49 @@ package com.example.projet_e_commerce;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ConnexionCommercantActivity extends AppCompatActivity {
+
+    Button bouttonClient;
+    Button bouttonGestionnaire;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connexion_commercant);
+
+        bouttonClient = findViewById(R.id.bouttonClient);
+        bouttonGestionnaire = findViewById(R.id.bouttonGestionnaire);
+        bouttonGestionnaire.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ConnexionGestionnaire();
+            }
+        });
+
+
+
+        bouttonClient.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ConnexionClient();
+            }
+        });
+
+    }
+
+    public void ConnexionClient(){
+        Intent intent = new Intent(this,ConnexionClientActivity.class);
+        startActivity(intent);
+    }
+
+    public void ConnexionGestionnaire(){
+        Intent intent = new Intent(this,ConnexionGestionnaireActivity.class);
+        startActivity(intent);
     }
 }
